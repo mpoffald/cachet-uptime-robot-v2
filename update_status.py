afe0d7c6c137672668cef51272a852558e0ad794
root <root@ip-172-31-28-252.us-west-2.compute.internal>
2017-12-02 00:23:28 +0000

    Uptimerobot integration, updated to use requests lib and basicauth

--- update_status.py
+++ update_status.py
@@ -4,23 +4,26 @@ import sys
 import time
 import configparser
 from urllib import request
-from urllib import parse
+from urllib import parse ##updating to requests library
+import requests
 from datetime import datetime
 
 
 class UptimeRobot(object):
     """ Intermediate class for setting uptime stats.
     """
-    def __init__(self, api_key):
+    def __init__(self, api_key, authuser, authpass):
         self.api_key = api_key
         self.base_url = 'https://api.uptimerobot.com/v2/getMonitors'
-
-    def get_monitors(self, response_times=0, logs=0, uptime_ratio=30):
+        self.authuser = authuser
+        self.authpass = authpass
+    def get_monitors(self, response_times=1, logs=1, uptime_ratio=30):
         """
         Returns status and response payload for all known monitors.
         """
+        print('GET MONITORS')
         endpoint = self.base_url
-        data = parse.urlencode({
+        data = {
                 'api_key': format(self.api_key),
                 'format': 'json',
                 # responseTimes - optional (defines if the response time data of each
@@ -34,22 +37,25 @@ class UptimeRobot(object):
                 # the uptime ratio(s) for. Ex: customUptimeRatio=7-30-45 to get the
                 # uptime ratios for those periods)
                 'custom_uptime_ratios': format(uptime_ratio)
-            }).encode('utf-8')
+            } ##NOTE used to be encoding here 
 
-        url = request.Request(
-            url=endpoint,
-            data=data,
-            method='POST',
-            headers={'content-type': "application/x-www-form-urlencoded",'cache-control': "no-cache"},
-        )
+
+        
+        headers={'content-type': "application/x-www-form-urlencoded",'cache-control': "no-cache"}
 
         # Verifying in the response is jsonp in otherwise is error
-        response = request.urlopen(url)
-        content = response.read().decode('utf-8')
-        j_content = json.loads(content)
+
+        response = requests.request('POST', endpoint, data=data, headers=headers, auth=(authuser, authpass))
+
+    
+
+        ##get rsponse and convert to json
+        j_content = response.json()
+
         if j_content.get('stat'):
             stat = j_content.get('stat')
             if stat == 'ok':
+
                 return True, j_content
 
         return False, j_content
@@ -69,9 +75,12 @@ class CachetHq(object):
     CACHET_SEEMS_DOWN = 3
     CACHET_DOWN = 4
 
-    def __init__(self, cachet_api_key, cachet_url):
+    def __init__(self, cachet_api_key, cachet_url, authuser, authpass):
         self.cachet_api_key = cachet_api_key
         self.cachet_url = cachet_url
+        self.authuser = authuser
+        self.authpass = authpass #request does not have "auth", try switching to requests (import requests and maybe update query syntax)
+
 
     def update_component(self, id_component=1, status=None):
         component_status = None
@@ -87,47 +96,67 @@ class CachetHq(object):
         # Down
         elif status == self.UPTIME_ROBOT_DOWN:
             component_status = self.CACHET_DOWN
-
+        print('UPDATE COMPONENT')
         if component_status:
             url = '{0}/api/v1/{1}/{2}'.format(
                 self.cachet_url,
                 'components',
                 id_component
             )
-            data = parse.urlencode({
+
+            data = {
                 'status': component_status,
-            }).encode('utf-8')
+            }
+
+            headers={'X-Cachet-Token': self.cachet_api_key}
+            '''
             req = request.Request(
                 url=url,
                 data=data,
                 method='PUT',
                 headers={'X-Cachet-Token': self.cachet_api_key},
+
             )
-            response = request.urlopen(req)
-            content = response.read().decode('utf-8')
-            return content
+            '''
+            response = requests.request('PUT', url, data=data, headers=headers, auth=(self.authuser, self.authpass))
+            print('REQUEST MADE')
+#            response = request.urlopen(req)
+#            content = response.read().decode('utf-8')
+            print('update compnent complete')
+ #           return content
+            return response
 
     def set_data_metrics(self, value, timestamp, id_metric=1):
+        print('SET DATA METRICS')
         url = '{0}/api/v1/metrics/{1}/points'.format(
             self.cachet_url,
             id_metric
         )
 
-        data = parse.urlencode({
+        print("metric url (should be 2): " + str(url))
+#        data = OAparse.urlencode({
+        data = {
             'value': value,
             'timestamp': timestamp,
-        }).encode('utf-8')
+        }
+        '''
         req = request.Request(
             url=url,
             data=data,
             method='POST',
             headers={'X-Cachet-Token': self.cachet_api_key},
         )
-        response = request.urlopen(req)
-
-        return json.loads(response.read().decode('utf-8'))
+        '''
+        headers={'X-Cachet-Token': self.cachet_api_key}
+        
+        response = requests.request('POST', url, data=data, headers=headers, auth=(self.authuser, self.authpass))
+        
+        print('response in set_data_metrics: ' + str(response))
+ #       return json.loads(response.read().decode('utf-8'))
+        return response.json()
 
     def get_last_metric_point(self, id_metric):
+        print('GET LAST METRIC POINT')
         url = '{0}/api/v1/metrics/{1}/points'.format(
             self.cachet_url,
             id_metric
@@ -136,8 +165,10 @@ class CachetHq(object):
         req = request.Request(
             url=url,
             method='GET',
-            headers={'X-Cachet-Token': self.cachet_api_key}
+            headers={'X-Cachet-Token': self.cachet_api_key},
         )
+
+        req.auth = self.auth
         response = request.urlopen(req)
         content = response.read().decode('utf-8')
 
@@ -156,6 +187,8 @@ class CachetHq(object):
             method='GET',
             headers={'X-Cachet-Token': self.cachet_api_key},
         )
+
+        req.auth=self.auth
         response = request.urlopen(req)
         content = response.read().decode('utf-8')
 
@@ -172,9 +205,11 @@ class CachetHq(object):
 
 
 class Monitor(object):
-    def __init__(self, monitor_list, api_key):
+    def __init__(self, monitor_list, api_key, authuser, authpass):
         self.monitor_list = monitor_list
         self.api_key = api_key
+        self.authuser = authuser
+        self.authpass = authpass
 
     def send_data_to_catchet(self, monitor):
         """ Posts data to Cachet API.
@@ -189,6 +224,8 @@ class Monitor(object):
         cachet = CachetHq(
             cachet_api_key=website_config['cachet_api_key'],
             cachet_url=website_config['cachet_url'],
+            authuser=self.authuser,
+            authpass=self.authpass,
         )
 
         if 'component_id' in website_config:
@@ -207,7 +244,7 @@ class Monitor(object):
     def update(self):
         """ Update all monitors uptime and status.
         """
-        uptime_robot = UptimeRobot(self.api_key)
+        uptime_robot = UptimeRobot(self.api_key, self.authuser, self.authpass)
         success, response = uptime_robot.get_monitors(response_times=1)
         if success:
             monitors = response.get('monitors')
@@ -234,19 +271,25 @@ if __name__ == "__main__":
 
     UPTIME_ROBOT_API_KEY = None
     MONITOR_DICT = {}
-    for element in SECTIONS:
-        if element == 'uptimeRobot':
-            uptime_robot_api_key = CONFIG[element]['UptimeRobotMainApiKey']
+    for section in SECTIONS:
+        if section == 'uptimeRobot':
+            uptime_robot_api_key = CONFIG[section]['UptimeRobotMainApiKey']
+        elif section == 'cachet_auth':
+            authuser = CONFIG[section]['Authuser']
+            authpass = CONFIG[section]['Authpass']
         else:
-            MONITOR_DICT[element] = {
-                'cachet_api_key': CONFIG[element]['CachetApiKey'],
-                'cachet_url': CONFIG[element]['CachetUrl'],
-                'metric_id': CONFIG[element]['MetricId'],
+            MONITOR_DICT[section] = {
+                'cachet_api_key': CONFIG[section]['CachetApiKey'],
+                'cachet_url': CONFIG[section]['CachetUrl'],
+                'metric_id': CONFIG[section]['MetricId'],
             }
-            if 'ComponentId' in CONFIG[element]:
-                MONITOR_DICT[element].update({
-                    'component_id': CONFIG[element]['ComponentId'],
+            if 'ComponentId' in CONFIG[section]:
+                MONITOR_DICT[section].update({
+                    'component_id': CONFIG[section]['ComponentId'],
+#                    'authuser':CONFIG[element]['Authuser'],
+#                    'authpass':CONFIG[element]['Authpass'],
                 })
+        
 
-    MONITOR = Monitor(monitor_list=MONITOR_DICT, api_key=uptime_robot_api_key)
+    MONITOR = Monitor(monitor_list=MONITOR_DICT, api_key=uptime_robot_api_key, authuser=authuser, authpass=authpass)
     MONITOR.update()
